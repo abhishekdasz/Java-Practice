692a2e2ac89e33e58a5068ed1fe4c860bec48a80
Abhishek Das <abhishekdaszy@gmail.com>
2024-06-02 17:13:59 +0530

    updated

--- Five.java
+++ Five.java
@@ -43,13 +43,36 @@ public class Five
             System.out.println(arr[i]);
         }
     }
+
+    public void ISort(int arr[])
+    {
+        for(int i=1; i<arr.length; i++)
+        {
+            int curr = arr[i];
+            int prev = i-1;
+            // finding out the correct pos to insert
+            while(prev>=0 && arr[prev]>curr)
+            {
+                arr[prev+1] = arr[prev];
+                prev--;   
+            }
+            // insertion
+            arr[prev+1] = curr;
+        }
+        for(int i=0; i<arr.length; i++)
+        {
+            System.out.println(arr[i]);
+        }
+    }
     public static void main(String[] args) 
     {
         int arr[] = {5,4,1,3,2};
         Five obj = new Five();
         // obj.BSort(arr);
 
-        obj.SSort(arr);
+        // obj.SSort(arr);
+
+        obj.ISort(arr);
 
 
     }    
