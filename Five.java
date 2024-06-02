18ad52bdfed2d234557627cb451ab5118b0d1bce
Abhishek Das <abhishekdaszy@gmail.com>
2024-06-02 15:41:46 +0530

    updated

---
+++ Five.java
@@ -0,0 +1,30 @@
+public class Five 
+{
+    public void BSort(int arr[])
+    {
+        for(int turn=0; turn<arr.length-1; turn++)
+        {
+            for(int j=0; j<arr.length-1-turn; j++)
+            {
+                if(arr[j]>arr[j+1])
+                {
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+        for(int i=0; i<arr.length; i++)
+        {
+            System.out.println(arr[i]);
+        }
+    }
+    public static void main(String[] args) 
+    {
+        int arr[] = {5,4,1,3,2};
+        Five obj = new Five();
+        obj.BSort(arr);
+
+
+    }    
+}
--- Four.java
+++ Four.java
@@ -1,4 +1,4 @@
-class Three
+public class Four 
 {
     public void re(int first, int last, int arr[])
     {
@@ -13,11 +13,14 @@ class Three
         }
         for(int i=0; i<arr.length; i++)
         {
-            System.out.println(arr[i]);
+            System.out.print(arr[i]+",");
         }
     }
     public static void main(String[] args) 
     {
-        int arr[]={1,2,3,4,5};    
+        int arr[]={1,2,3,4,5};  
+
+        Four obj = new Four();
+        obj.re(0, arr.length-1, arr);
     }
 }
