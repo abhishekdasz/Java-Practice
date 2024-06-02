99508cecd8b166b0110bf20289fc0cabf4508159
Abhishek Das <abhishekdaszy@gmail.com>
2024-06-02 16:37:09 +0530

    updated

--- Five.java
+++ Five.java
@@ -19,11 +19,37 @@ public class Five
             System.out.println(arr[i]);
         }
     }
+
+
+    public void SSort(int arr[])
+    {
+        for(int i=0; i<arr.length-1; i++)
+        {
+            int minPos = i;
+            for(int j=i+1; j<arr.length; j++)
+            {
+                if(arr[minPos] > arr[j])
+                {
+                    minPos = j;
+                }
+            }
+            // swap
+            int temp = arr[minPos];
+            arr[minPos] = arr[i];
+            arr[i] = temp;
+        }
+        for(int i=0; i<arr.length; i++)
+        {
+            System.out.println(arr[i]);
+        }
+    }
     public static void main(String[] args) 
     {
         int arr[] = {5,4,1,3,2};
         Five obj = new Five();
-        obj.BSort(arr);
+        // obj.BSort(arr);
+
+        obj.SSort(arr);
 
 
     }    
