a564929ee4580ea897f3112e63fc892b0b5b6f3a
Abhishek Das <abhishekdaszy@gmail.com>
2024-06-10 23:44:10 +0530

    updated

---
+++ Anagram.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+
+public class Anagram 
+{
+    public static void main(String[] args) 
+    {
+        String a = "Listen";
+        String b = "Silent";
+
+        a = a.toLowerCase();
+        b = b.toLowerCase();
+
+        char [] chArr1 = a.toCharArray();
+        char [] chArr2 = a.toCharArray();
+        
+        Arrays.sort(chArr1);
+        Arrays.sort(chArr2);
+
+        if(chArr1.equals(chArr2))
+        {
+            System.out.println("Anagram");
+        }
+    }     
+}
