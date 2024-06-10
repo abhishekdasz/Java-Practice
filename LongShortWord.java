978386e9084bbb42e5dd5ba42650c600058ed71c
Abhishek Das <abhishekdaszy@gmail.com>
2024-06-10 22:13:26 +0530

    updated

---
+++ LongShortWord.java
@@ -0,0 +1,27 @@
+public class LongShortWord 
+{
+    public static void main(String[] args) 
+    {
+        String s = "Hello World My name is Abhishek";
+
+        String words [] = s.split("\\s");
+
+        String longestWord = "";
+        String shortestWord = words[0];
+
+        for(int i = 0; i<words.length; i++)
+        {
+            String word = words[i];
+            if(word.length() > longestWord.length())
+            {
+                longestWord = word;
+            }
+            if(word.length() < shortestWord.length())
+            {
+                shortestWord = word;
+            }
+        }
+        System.out.println("Longest Word:" + longestWord);
+        System.out.println("Shortest Word:" + shortestWord);
+    }
+}
