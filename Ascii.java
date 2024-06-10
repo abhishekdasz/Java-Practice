8b652ebe4de8b2d9d7cc1fd8eb374e385cfab0ee
Abhishek Das <abhishekdaszy@gmail.com>
2024-06-10 18:40:11 +0530

    updated

---
+++ Ascii.java
@@ -0,0 +1,13 @@
+public class Ascii 
+{
+    public static void main(String[] args) 
+    {
+        char ch1 = 'a';
+        char ch2 = 'A';
+
+        int asciiVal1 = ch1;
+        int asciiVal2 = ch2;
+
+        System.out.println(asciiVal1 + " " + asciiVal2);
+    }    
+}
