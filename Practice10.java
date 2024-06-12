623d2c0e68b7d007aaabbf3135f1299456d8a727
Abhishek Das <abhishekdaszy@gmail.com>
2024-06-13 00:03:11 +0530

    updated

--- Practice10.java
+++ Practice10.java
@@ -7,15 +7,22 @@ public class Practice10
         String s1 = "listen";
         String s2 = "silent";
 
+        s1.toLowerCase();
+        s2.toLowerCase();
+
         char [] ch1 = s1.toCharArray();
         char [] ch2 = s2.toCharArray();
 
         Arrays.sort(ch1);
         Arrays.sort(ch2);
 
-        if(ch1.equals(ch2))
+        if(Arrays.equals(ch1, ch2))
         {
             System.out.println("Both strings are anagram to each other.");
+        }
+        else
+        {
+            System.out.println("Not Anagram");
         }
     }    
 }
