858651256cae948d5ec81492d3de516ee7e47bdc
Abhishek Das <abhishekdaszy@gmail.com>
2024-06-13 01:02:59 +0530

    updated

---
+++ Practice13.java
@@ -0,0 +1,11 @@
+public class Practice13 
+{
+    public static void main(String[] args) 
+    {
+        String s = "Hello World";
+
+        String words[] = s.split("\\s");
+
+        System.out.println(words.length);
+    }    
+}
