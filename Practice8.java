db480d23824293ddd85cf1851767c4769bba1b9b
Abhishek Das <abhishekdaszy@gmail.com>
2024-06-12 21:52:25 +0530

    updated

--- Practice8.java
+++ Practice8.java
@@ -7,17 +7,18 @@ public class Practice8
         for(int i = 0; i < s.length(); i++)
         {
             boolean isRepeated = false;
-            for(int j = 0; j < s.length(); j++)
+            for(int j=0; j<s.length(); j++)
             {
-                if(i != j && s.charAt(i) == s.charAt(j))
+                if(i!=j && s.charAt(i) == s.charAt(j))
                 {
                     isRepeated = true;
                     break;
                 }
             }
+
             if(!isRepeated)
             {
-                System.out.print(s.charAt(i) + " ");
+                System.out.print(s.charAt(i) +" ");
             }
         }
     }    
