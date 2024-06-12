f60d6e59fb92f410b00406e20e7ce1fd6e49d293
Abhishek Das <abhishekdaszy@gmail.com>
2024-06-12 23:10:20 +0530

    updated

---
+++ Practice10.java
@@ -0,0 +1,19 @@
+public class Practice10 
+{
+    public static void main(String[] args) 
+    {
+        String s1 = "listen";
+        String s2 = "silent";
+        
+        for(int i=0; i<s1.length(); i++)
+        {
+            System.out.println(s1.charAt(i));
+
+            for(int j=0; j<s2.length(); j++)
+            {
+                System.out.println(s2.charAt(j));
+            }
+        }
+    }    
+}
+  
