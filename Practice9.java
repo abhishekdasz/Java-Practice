c2a30954e5c33a1443e84e11be9c511e79042ced
Abhishek Das <abhishekdaszy@gmail.com>
2024-06-12 22:01:58 +0530

    updated

--- Practice9.java
+++ Practice9.java
@@ -2,6 +2,27 @@ public class Practice9
 {
     public static void main(String[] args) 
     {
-            
+        String s = "Hello World, This is Abhishek Das";
+        
+        String words [] = s.split("\\s");
+
+        String smallestWord = words[0];
+        String longestWord = "";
+
+        for(int i=0; i<words.length; i++)
+        {
+            String word = words[i];
+
+            if(word.length() < smallestWord.length())
+            {
+                smallestWord = word;
+            }
+            if(word.length() > longestWord.length())
+            {
+                longestWord = word;
+            }
+
+            // System.out.println(word.length());
+        }
     }    
 }
