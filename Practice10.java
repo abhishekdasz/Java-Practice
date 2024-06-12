548d9ad08ff77a326a8953094f20d8de9035e087
Abhishek Das <abhishekdaszy@gmail.com>
2024-06-12 23:55:43 +0530

    updated

--- Practice10.java
+++ Practice10.java
@@ -3,20 +3,10 @@ public class Practice10
     public static void main(String[] args) 
     {
         String s1 = "listen";
+        String s2 = "silent";
 
-
-        
-
-        char cha1 [] = new char[s1.length()];
-        for(int i=0; i<s1.length(); i++)
-        {
-            cha1[i] = s1.charAt(i);
-        }
-
-        for(int i=0; i<cha1.length; i++)
-        {
-            System.out.println(cha1[i]);
-        }
+        char [] ch1 = s1.toCharArray();
+        char [] ch2 = s2.toCharArray();
     }    
 }
   
