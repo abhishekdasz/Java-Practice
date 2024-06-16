4350f4a37d98bbaf097d7f4cba53a98f06c3905d
Abhishek Das <abhishekdaszy@gmail.com>
2024-06-16 23:41:16 +0530

    updated

--- Pr10.java
+++ Pr10.java
@@ -1,3 +1,18 @@
-public class Pr10 {
-    
+class StaticBlock 
+{
+    static
+    {
+        System.out.println("Hello World from Static Block.");
+    }
+}
+public class Pr10 
+{
+    static
+    {
+        System.out.println("Hello World");
+    }
+    public static void main(String[] args) 
+    {
+        StaticBlock s = new StaticBlock();
+    }    
 }
