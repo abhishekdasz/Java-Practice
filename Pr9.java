d2a480726495a447a40fa34da222619feceac696
Abhishek Das <abhishekdaszy@gmail.com>
2024-06-16 22:42:49 +0530

    updated

--- Pr9.java
+++ Pr9.java
@@ -1,7 +1,21 @@
+class A 
+{
+    int x = 10;    
+}
+class B extends A
+{
+    int x = 20;
+    void printX()
+    {
+        System.out.println("This x:"+ this.x);
+        System.out.println("Super x:"+ super.x);
+    }
+}
 public class Pr9 
 {
     public static void main(String[] args) 
     {
-        System.out.println();    
+        B obj = new B();  
+        obj.printX();  
     }    
 }
